6a3adf3665e76f0b03f1f52822d0150c6db8d5e5
sifat <sifatulla.haque@gmail.com>
2022-06-20 15:20:03 +0600

    saving the file just

---
+++ day1.py
@@ -0,0 +1,26 @@
+print("Hello world")
+a=21
+b=23
+print (a+b)
+#+,-,*,/ are called arithmatic operator
+print("Arithmatic operator 1(+); a+b=",a+b )
+print("Arithmatic operator 2 (-); a-b=", a-b)
+print("Arithmatic operator 3 (*);a*b=",a*b)
+print("Arithmatic operator 3 (/);a/b=",a/b)
+#Assignment operators
+c=21
+c+=5
+c-=7
+c*=8
+c/=8
+print(c)
+#<,>,<=,>=, == , != are called comparision operators. it return a bullian nnumber
+d=(7>4)
+e= (56==56)
+print(d)
+# and or not are call logical operator 
+bull1=True
+bull2=False
+print("bull1 and bull 2 is ", bull1 and bull2)
+print("bull1 or bull2 is ", bull1 or bull2)
+print("not of bull1", not bull1)
